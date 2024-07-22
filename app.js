f39e56226a3e4f34acc001f9e6e45cf19e9d9af6
Abhishek <abhishekamera1@gmail.com>
2024-07-22 11:11:57 +0530

    Merge auth into main

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const cors = require('cors');
+const userRoute = require('./routes/userRoute')
 const app = express();
+
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+//Mounting routes
+app.use('/user',userRoute);
 
 app.use((req, res, next) => {
     console.log('Hello from Middleware ');
     next();
 });
-
-
-
 
 
 module.exports = app; 
